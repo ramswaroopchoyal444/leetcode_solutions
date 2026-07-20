a0b07f0142df7d485316141eb02278f2b64f82bb
ramswaroopchoyal444 <ramswaroopchoyal444@gmail.com>
2026-07-20 23:19:03 +0530

    Time: 11 ms (30.05%) | Memory: 10.9 MB (71.26%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -12,13 +12,34 @@ public:
 
         while(next_indx < s.size()){
 
-            maxfreq = max(maxfreq, ++freq[s[next_indx]]);
+            freq[s[next_indx]]++;
+
+            maxfreq = max(maxfreq, freq[s[next_indx]]);
+
+            char maxfreqChar;
+
+            if(maxfreq == freq[s[next_indx]]){
+                maxfreqChar = s[next_indx];
+            }
 
             next_indx++;
 
             while((next_indx-previous_indx) - maxfreq > k){
 
                 freq[s[previous_indx]]--;
+
+                if(char(s[previous_indx]) == maxfreqChar){
+                    maxfreq--;
+                }
+
+                // Now here I've to check frequency of each element in map to update the maxfreq and maxfreqChar.
+
+                for(auto it : freq){
+                    if(it.second > maxfreq){
+                        maxfreq = it.second;
+                        maxfreqChar = it.first;
+                    }
+                }
 
                 previous_indx++;
             }
