2f39ca2c476e375fb387ff3a795b3076c18024b3
ramswaroopchoyal444 <ramswaroopchoyal444@gmail.com>
2026-08-19 11:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (58.04%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -4,29 +4,21 @@ public:
         
         vector<int> ans;
 
-        unordered_set<int> s1, s2, un;
+        unordered_set<int> s, s1;
 
         for(int i = 0; i < nums1.size(); i++){
-            s1.insert(nums1[i]);
-            un.insert(nums1[i]);
+            s.insert(nums1[i]);
         }
 
         for(int i = 0; i < nums2.size(); i++){
-            s2.insert(nums2[i]);
-            un.insert(nums2[i]);
-        }
-
-        for(int i : un){
-
-            bool x = (s1.find(i) != s1.end());
-            bool y = (s2.find(i) != s2.end());
-
-            if(x && y){
-                ans.push_back(i);
+            if(s.find(nums2[i]) != s.end()){
+                if(s1.find(nums2[i]) == s1.end()){
+                    ans.push_back(nums2[i]);
+                }
+                s1.insert(nums2[i]);
             }
         }
 
         return ans;
-
     }
 };
