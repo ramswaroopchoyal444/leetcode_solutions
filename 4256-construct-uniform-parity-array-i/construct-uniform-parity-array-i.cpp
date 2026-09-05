62a8474759ca2230c3f0cb3e80304660634fa02c
ramswaroopchoyal444 <ramswaroopchoyal444@gmail.com>
2026-09-05 18:51:38 +0530

    Time: 1 ms (8.77%) | Memory: 30.2 MB (66.47%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -2,51 +2,7 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         
-        int n = nums1.size();
-
-        int oddCount = 0, evenCount = 0;
-
-        int min_even = 102, min_odd = 101;
-
-        for(int i = 0; i < n; i++){
-
-            if(nums1[i] % 2 == 0){
-                // Even number - 
-
-                min_even = min(min_even, nums1[i]);
-
-                evenCount++;
-            }else{
-                // odd number - 
-
-                min_odd = min(min_odd, nums1[i]);
-
-                oddCount++;
-            }
-        }
-
-        if(evenCount == n || oddCount == n){
-            return true;
-        }
-
-        // int madeEven = 0, madeOdd = 0;
-
-        // for(int i = 0; i < n; i++){
-
-        //     if(nums1[i] % 2 == 0){
-
-        //         // Even number - 
-
-        //         evenCount++;
-
-        //         // Making it odd - 
-
-
-        //     }
-        // }
 
         return true;
-
-
     }
 };
